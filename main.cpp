32d606b64173dc10da304b583f42e88349947a39
Seva Lander <sevalander@gmail.com>
2018-05-11 14:05:55 +0300

    Инициализация AndorSDK, проверка наличия камеры.

---
+++ main.cpp
@@ -0,0 +1,60 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+#ifdef __GNUC__
+#  if(__GNUC__ > 3 || __GNUC__ ==3)
+#	define _GNUC3_
+#  endif
+#endif
+
+#ifdef _GNUC3_
+#  include <iostream>
+#  include <fstream>
+   using namespace std;
+#else
+#  include <iostream.h>
+#  include <fstream.h>
+#endif
+
+#include <unistd.h>
+
+#include "atmcdLXd.h"
+
+int CameraSelect (int iNumArgs, char* szArgList[]);
+
+int main(int argc, char* argv[]){
+	if (CameraSelect (argc, argv) < 0) {
+      cout << "*** CAMERA SELECTION ERROR" << endl;
+      return -1;
+    }
+
+	//Initialize CCD
+	error = Initialize("/usr/local/etc/andor");
+	if(error!=DRV_SUCCESS){
+		cout << "Initialisation error...exiting" << endl;
+		return(1);
+	}
+
+	sleep(2); //sleep to allow initialization to complete
+	return 0;
+}
+
+int CameraSelect (int iNumArgs, char* szArgList[])
+{
+  if (iNumArgs == 2) {
+ 
+    at_32 lNumCameras;
+    GetAvailableCameras(&lNumCameras);
+    int iSelectedCamera = atoi(szArgList[1]);
+ 
+    if (iSelectedCamera < lNumCameras && iSelectedCamera >= 0) {
+      at_32 lCameraHandle;
+      GetCameraHandle(iSelectedCamera, &lCameraHandle);
+      SetCurrentCamera(lCameraHandle);
+      return iSelectedCamera;
+    }
+    else
+      return -1;
+  }
+  return 0;
+}
