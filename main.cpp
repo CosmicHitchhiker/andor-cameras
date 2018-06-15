6da48b8865d8e0dbc680ce34c3a01641198852c1
Seva Lander <sevalander@gmail.com>
2018-06-15 18:19:45 +0300

    Just some interim code.

--- main.cpp
+++ main.cpp
@@ -18,43 +18,71 @@
 
 #include <unistd.h>
 
+#include "fitsio.h"
+
 #include "atmcdLXd.h"
 
-int CameraSelect (int iNumArgs, char* szArgList[]);
+int init();
+int loop();
 
 int main(int argc, char* argv[]){
-	if (CameraSelect (argc, argv) < 0) {
-      cout << "*** CAMERA SELECTION ERROR" << endl;
-      return -1;
-    }
+	int status = 0;
 
-	//Initialize CCD
-	error = Initialize("/usr/local/etc/andor");
+	status = init();
+
+	// do {
+	// 	status = loop();
+	// } while (status == -1);
+
+	return 0;
+}
+
+int init(){
+    unsigned long error;
+    int xpix, ypix;
+    char camera_name[1024];
+    int desired_temperature = 0;
+    int current_temperature = 0;
+
+    //Initialize CCD
+    error = Initialize("/usr/local/etc/andor");
 	if(error!=DRV_SUCCESS){
 		cout << "Initialisation error...exiting" << endl;
 		return(1);
 	}
+	sleep(2);
 
-	sleep(2); //sleep to allow initialization to complete
+	error = GetDetector(&xpix, &ypix);
+
+	// error = GetHeadModel(&camera_name);
+
+	do {
+		cout << "T: ";
+		cin >> desired_temperature;
+		error = SetTemperature(desired_temperature);
+	} while (error == DRV_P1INVALID);
+
+	error = CoolerON();
+
+	do {
+		error = GetTemperature(&current_temperature);
+		cout << "\rCurrent temperature is " << current_temperature << "       ";
+		sleep(1);
+	} while (error != DRV_TEMP_STABILIZED);
+
+	cout << endl << "Desired temperature is achieved!" << endl;
+
+	desired_temperature = 15;
+	error = SetTemperature(desired_temperature);
+	error = CoolerOFF();
+
+	do {
+		error = GetTemperature(&current_temperature);
+		cout << "\rCurrent temperature is " << current_temperature << "      ";
+	} while (current_temperature < desired_temperature);
+
+	error = ShutDown();
+
 	return 0;
 }
 
-int CameraSelect (int iNumArgs, char* szArgList[])
-{
-  if (iNumArgs == 2) {
- 
-    at_32 lNumCameras;
-    GetAvailableCameras(&lNumCameras);
-    int iSelectedCamera = atoi(szArgList[1]);
- 
-    if (iSelectedCamera < lNumCameras && iSelectedCamera >= 0) {
-      at_32 lCameraHandle;
-      GetCameraHandle(iSelectedCamera, &lCameraHandle);
-      SetCurrentCamera(lCameraHandle);
-      return iSelectedCamera;
-    }
-    else
-      return -1;
-  }
-  return 0;
-}
