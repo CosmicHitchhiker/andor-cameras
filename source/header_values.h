670c06b0a4351f6ca0deb71bfca19dda6e0c7e60
Seva <sevalander@gmail.com>
2020-01-12 17:47:30 +0300

    Add HeaderValues class
    Class to store header values to write and to delete. It also applies this changes to given fits file.

---
+++ source/header_values.h
@@ -0,0 +1,29 @@
+#include <vector>
+#include <string>
+// #include <variant>
+#include "fitsio.h"
+#include <boost/algorithm/string.hpp>
+
+#ifndef HEADER_VALUES_H
+#define HEADER_VALUES_H
+
+class HeaderValues
+{
+  public:
+	HeaderValues();
+	~HeaderValues();
+	void addKey(std::string key, char type = 's', std::string value = "", std::string comment = "");
+	void parseString(std::string str);
+	void printAll();
+	void update(std::string filename);
+
+  private:
+  	int n; // Количество ключей для добавления
+  	std::vector<std::string> keys;
+  	std::vector<char> types;
+  	// std::vector<std::variant<int, double, std::string, float>> values;
+  	std::vector<std::string> values;
+  	std::vector<std::string> comments;
+};
+
+#endif // HEADER_VALUES_H
